e61488cad01a2080300f7f979290c89afa388c15
LockedOnU <lockedonu@gmail.com>
2018-05-28 15:05:56 +0900

    watchEnvService

--- Local_Server/src/local/WatchEnvironmentService.java
+++ Local_Server/src/local/WatchEnvironmentService.java
@@ -1,27 +1,14 @@
 package local;
-
-import java.util.List;
-import java.util.ArrayList;
- 
 import com.pi4j.wiringpi.Gpio;
 import com.pi4j.wiringpi.GpioUtil;
 
 public class WatchEnvironmentService implements Runnable {
 	private Environment env;
-	
 	private static final int MAXTIMINGS = 85;
     private int[] dht11_dat = { 0, 0, 0, 0, 0 };
     
-    public Environment getEnv() {
-		return env;
-	}
-
-	public void setEnv(Environment env) {
+    public WatchEnvironmentService(Environment env) {
 		this.env = env;
-	}
-	
-	public WatchEnvironmentService(Environment env) {
-		this.setEnv(env);
 		
 		// setup wiringPi
         if (Gpio.wiringPiSetup() == -1) {
@@ -29,89 +16,91 @@ public class WatchEnvironmentService implements Runnable {
             return;
         }
  
-       GpioUtil.export(3, GpioUtil.DIRECTION_OUT);     
+       GpioUtil.export(3, GpioUtil.DIRECTION_OUT);  
 	}
-	
-	public void getTemperature() {
-	       int laststate = Gpio.HIGH;
-	       int j = 0;
-	       dht11_dat[0] = dht11_dat[1] = dht11_dat[2] = dht11_dat[3] = dht11_dat[4] = 0;
-	       StringBuilder value = new StringBuilder();
-	 
-	       Gpio.pinMode(3, Gpio.OUTPUT);
-	       Gpio.digitalWrite(3, Gpio.LOW);
-	       Gpio.delay(18);
-	 
-	       Gpio.digitalWrite(3, Gpio.HIGH);       
-	       Gpio.pinMode(3, Gpio.INPUT);
-	 
-	       for (int i = 0; i < MAXTIMINGS; i++) {
-	          int counter = 0;
-	          while (Gpio.digitalRead(3) == laststate) {
-	              counter++;
-	              Gpio.delayMicroseconds(1);
-	              if (counter == 255) {
-	                  break;
-	              }
-	          }
-	 
-	          laststate = Gpio.digitalRead(3);
-	 
-	          if (counter == 255) {
-	              break;
-	          }
-	 
-	          /* ignore first 3 transitions */
-	          if ((i >= 4) && (i % 2 == 0)) {
-	             /* shove each bit into the storage bytes */
-	             dht11_dat[j / 8] <<= 1;
-	             if (counter > 16) {
-	                 dht11_dat[j / 8] |= 1;
-	             }
-	             j++;
-	           }
-	        }
-	        // check we read 40 bits (8bit x 5 ) + verify checksum in the last
-	        // byte
-	        if ((j >= 40) && checkParity()) {
-	            float h = (float)((dht11_dat[0] << 8) + dht11_dat[1]) / 10;
-	            if ( h > 100 )
-	            {
-	                h = dht11_dat[0];   // for DHT11
-	            }
-	            float c = (float)(((dht11_dat[2] & 0x7F) << 8) + dht11_dat[3]) / 10;
-	            if ( c > 125 )
-	            {
-	                c = dht11_dat[2];   // for DHT11
-	            }
-	            if ( (dht11_dat[2] & 0x80) != 0 )
-	            {
-	                c = -c;
-	            }
-	            float f = c * 1.8f + 32;
-	            System.out.println( "Humidity = " + h + " Temperature = " + c + "(" + f + "f)");
-	        }else  {
-	            System.out.println( "Data not good, skip" );
-	        }
-	 
-	    }
-	
-	private boolean checkParity() {
-	      return (dht11_dat[4] == ((dht11_dat[0] + dht11_dat[1] + dht11_dat[2] + dht11_dat[3]) & 0xFF));
-	}
-
-	
+ 
+    public void getTemperature() {
+       int laststate = Gpio.HIGH;
+       int j = 0;
+       dht11_dat[0] = dht11_dat[1] = dht11_dat[2] = dht11_dat[3] = dht11_dat[4] = 0;
+       StringBuilder value = new StringBuilder();
+ 
+       Gpio.pinMode(3, Gpio.OUTPUT);
+       Gpio.digitalWrite(3, Gpio.LOW);
+       Gpio.delay(18);
+ 
+       Gpio.digitalWrite(3, Gpio.HIGH);       
+       Gpio.pinMode(3, Gpio.INPUT);
+ 
+       for (int i = 0; i < MAXTIMINGS; i++) {
+          int counter = 0;
+          while (Gpio.digitalRead(3) == laststate) {
+              counter++;
+              Gpio.delayMicroseconds(1);
+              if (counter == 255) {
+                  break;
+              }
+          }
+ 
+          laststate = Gpio.digitalRead(3);
+ 
+          if (counter == 255) {
+              break;
+          }
+ 
+          /* ignore first 3 transitions */
+          if ((i >= 4) && (i % 2 == 0)) {
+             /* shove each bit into the storage bytes */
+             dht11_dat[j / 8] <<= 1;
+             if (counter > 16) {
+                 dht11_dat[j / 8] |= 1;
+             }
+             j++;
+           }
+        }
+        // check we read 40 bits (8bit x 5 ) + verify checksum in the last
+        // byte
+        if ((j >= 40) && checkParity()) {
+            float h = (float)((dht11_dat[0] << 8) + dht11_dat[1]) / 10;
+            if ( h > 100 )
+            {
+                h = dht11_dat[0];   // for DHT11
+            }
+            float c = (float)(((dht11_dat[2] & 0x7F) << 8) + dht11_dat[3]) / 10;
+            if ( c > 125 )
+            {
+                c = dht11_dat[2];   // for DHT11
+            }
+            if ( (dht11_dat[2] & 0x80) != 0 )
+            {
+                c = -c;
+            }
+            float f = c * 1.8f + 32;
+            System.out.println( "Humidity = " + h + " Temperature = " + c + "(" + f + "f)");
+        }else  {
+            System.out.println( "Data not good, skip" );
+        }
+ 
+    }
+ 
+    private boolean checkParity() {
+      return (dht11_dat[4] == ((dht11_dat[0] + dht11_dat[1] + dht11_dat[2] + dht11_dat[3]) & 0xFF));
+    }
+    
 	@Override
 	public void run() {
 		//온도, 습도 감시
 		System.out.println("온도, 습도 감지 시작");
-		
-		WatchEnvironmentService dht = new WatchEnvironmentService(getEnv());
-		 
-        for (int i=0; i<10; i++) {
-           dht.getTemperature();
-        }
- 
-        System.out.println("Done!!");
+		try {
+			WatchEnvironmentService dht = new WatchEnvironmentService(env);
+			 
+			for (int i=0; i<10; i++) {
+			    Thread.sleep(2000);
+			    dht.getTemperature();
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("Done!!");
 	}
 }
